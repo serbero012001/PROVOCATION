9bf763e813d0557f31604ea869fafacd82431883
Alejandro Villa <tu-correo@ejemplo.com>
2026-03-14 12:29:34 -0500

    export mode

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig = {
-  /* config options here */
-  typescript: {
-    // Ignora errores de TypeScript al construir
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    // Ignora errores de ESLint al construir
-    ignoreDuringBuilds: true,
-  },
-} as any; // <--- El "as any" es el truco para que el error rojo desaparezca
+  output: 'export', // <--- ESTA LÍNEA ES VITAL
+  typescript: { ignoreBuildErrors: true },
+  eslint: { ignoreDuringBuilds: true },
+  images: { unoptimized: true },
+} as any;
 
 export default nextConfig;
